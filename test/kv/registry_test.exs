843c1202baf2170decff3b13fd737ec443939ffe
Tom Martin <tom@tpgmartin.com>
2017-07-13 17:03:59 +0100

    Add monitoring

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -15,4 +15,12 @@ defmodule KV.RegistryTest do
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
   end
+
+  test "remove buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
